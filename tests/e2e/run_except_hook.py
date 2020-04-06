7a162d991b2058b49742c5fa5f6df5635ed60d05
ksamuel <kevin.samuel@yandex.com>
2020-04-06 18:12:41 +0200

    Add e2e test for excepthook

---
+++ tests/e2e/run_except_hook.py
@@ -0,0 +1,14 @@
+import sys
+
+from postscriptum.excepthook import register_exception_handler
+
+
+def handler(type_, exception, traceback, previous_handler):
+    print("handled")
+
+
+raise_again = len(sys.argv[1:])
+
+register_exception_handler(handler, call_previous_handler=raise_again)
+
+1 / 0
---
+++ tests/e2e/test_excepthook_in_process.py
@@ -0,0 +1,21 @@
+import sys
+
+from subprocess import Popen, PIPE
+from pathlib import Path
+
+TEST_SCRIPT = Path(__file__).absolute().parent / "run_except_hook.py"
+
+
+def test_register():
+
+    process = Popen([sys.executable, TEST_SCRIPT], stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    assert stdout == b"handled\n", "New handler is called"
+    assert not stderr, "Previous handler is not called"
+
+    process = Popen(
+        [sys.executable, TEST_SCRIPT, "call_previous_handler"], stdout=PIPE, stderr=PIPE
+    )
+    stdout, stderr = process.communicate()
+    assert stdout == b"handled\n", "New handler is called"
+    assert b"ZeroDivisionError" in stderr, "Previous handler is called"
